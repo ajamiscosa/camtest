327a9f4d6ded4c1472091afc1ebb3b7dc9b2a53e
ajamiscosa <ajamiscosa@gmail.com>
2019-04-06 21:35:27 +0800

    initial

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+//AForge.Video dll
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Imaging;
+using AForge.Imaging.Filters;
+using AForge;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private FilterInfoCollection CaptureDevice; // list of webcam
+        private VideoCaptureDevice FinalFrame;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CaptureDevice = new FilterInfoCollection(FilterCategory.VideoInputDevice);//constructor
+            foreach (FilterInfo Device in CaptureDevice)
+            {
+                comboBox1.Items.Add(Device.Name);
+            }
+
+            comboBox1.SelectedIndex = 0; // default
+            FinalFrame = new VideoCaptureDevice();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FinalFrame = new VideoCaptureDevice(CaptureDevice[comboBox1.SelectedIndex].MonikerString);// specified web cam and its filter moniker string
+            FinalFrame.NewFrame += new NewFrameEventHandler(FinalFrame_NewFrame);// click button event is fired, 
+            FinalFrame.Start();
+        }
+
+        void FinalFrame_NewFrame(object sender, NewFrameEventArgs eventArgs) // must be void so that it can be accessed everywhere.
+                                                                             // New Frame Event Args is an constructor of a class
+        {
+            pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();// clone the bitmap
+        }
+
+        private void From1_CLosing(object sender, EventArgs e)
+        {
+            if (FinalFrame.IsRunning == true) FinalFrame.Stop();
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Filter += "JPEG (*.jpeg)|*.jpeg";
+                if(sfd.ShowDialog()==DialogResult.OK)
+                {
+                    pictureBox1.Image.Save(@sfd.FileName, ImageFormat.Jpeg);
+                    MessageBox.Show("saved");
+
+                    FinalFrame = new VideoCaptureDevice(CaptureDevice[comboBox1.SelectedIndex].MonikerString);// specified web cam and its filter moniker string
+                    FinalFrame.NewFrame += new NewFrameEventHandler(FinalFrame_NewFrame);// click button event is fired, 
+                    FinalFrame.Start();
+                }
+
+            }
+            else
+            { MessageBox.Show("null exception"); }
+        }
+
+        private void clone_Click(object sender, EventArgs e)
+        {
+            if (FinalFrame.IsRunning == true) FinalFrame.Stop();
+
+            Bitmap image = (Bitmap)pictureBox1.Image.Clone();
+
+            Rectangle cloneRect = new Rectangle((image.Width/2) - 128, (image.Height/2) - 128, 256, 256);
+            System.Drawing.Imaging.PixelFormat format =
+                image.PixelFormat;
+            Bitmap cloneBitmap = image.Clone(cloneRect, format);
+            
+            pictureBox1.Image = image.Clone(cloneRect,format);
+        }
+
+    }
+}
